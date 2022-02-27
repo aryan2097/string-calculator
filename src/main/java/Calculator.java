369ad177a8481c48dc1273195e4a705b114657fe
aryan2097 <53165020+aryan2097@users.noreply.github.com>
2022-02-27 23:48:06 +0530

    2nd use-case commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,8 @@
 public class Calculator {
     public static int add(String string) {
+        if(!string.isEmpty()){
+            return Integer.parseInt(string);
+        }
         return 0;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,4 +6,10 @@ public class CalculatorTest {
     void  should_return_zero_for_string() {
         Assertions.assertThat(Calculator.add("")).isEqualTo(0);
     }
+
+    @Test
+    void should_return_number_for_single_number() {
+        Assertions.assertThat(Calculator.add("10")).isEqualTo(10);
+    }
+
 }
